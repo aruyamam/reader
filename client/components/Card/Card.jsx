1405bfd1fbfb153afb0178f342c866ab256a09bf
aruyamam <aruyamam@gmail.com>
2019-04-12 16:12:50 +0900

    Cardコンポーネントにsizeプロップを追加

--- client/components/Card/Card.jsx
+++ client/components/Card/Card.jsx
@@ -3,12 +3,26 @@ import PropTypes from 'prop-types';
 import classes from './Card.css';
 import List from '../../helper/List';
 
-const Card = ({ children, className }) => {
+const sizes = {
+   small: 'small',
+   medium: 'medium',
+   big: 'big',
+   bleed: 'bleed',
+};
+
+const Card = ({ children, className, size }) => {
    const classList = new List(classes.card);
+   let cardSize = size;
+
+   if (!Object.keys(sizes).includes(size)) {
+      cardSize = sizes.medium;
+   }
 
    if (className) {
       classList.add(className);
    }
+
+   classList.add(classes[cardSize]);
 
    return <div className={classList.strList}>{children}</div>;
 };
